5f4f5c105a9fccdb908e3e5ca3e0dcda49fa4994
christian soto <cjsotoparra@email.arizona.edu>
2020-07-05 13:24:57 -0700

    Fixed bug in quicksort

--- Sort/quickSort.js
+++ Sort/quickSort.js
@@ -1,28 +1,38 @@
-function quickSort(A, p, r){
+const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
 
-	if(p < r){
-		q = partition(A,p,r);
-		quickSort(A, p, q-1);
-		quickSort(A, q+1, r);
+function quickSort(array, low, high){
+
+	if(low < high){
+		pIndex = partition(array,low,high);
+		quickSort(array, low, pIndex-1);
+		quickSort(array, pIndex+1, high);
 	}
 }
 
-function partition(A, p, r){
+function partition(array, low, high){
 
-	let x = A[r];
-	let i = p-1;
+	let pivot = array[high];
+	let i = low-1;
 	let temp = 0;
-	for(let j=p; j < r-1; j++){
-		if(A[j] <= x){
-			i = i+1;
-			temp = A[i];
-			A[i] = A[j];
-			A[j] = temp;
+	for(let j=low; j < high; j++){
+		//if current element is smaller than the pivot
+		if(array[j] < pivot){
+			i++;
+			//swap arr[i] and array[j]
+			temp = array[i];
+			array[i] = array[j];
+			array[j] = temp;
 		}
 	}
 
-	temp = A[i+1];
-	A[i+1] = A[r];
-	A[r] = A[i+1];
+	//swap array[i+1] and array[high] or pivot
+	temp = array[i+1];
+	array[i+1] = array[high];
+	array[high] = temp;
+
 	return i+1;
 }
+
+console.log(numbers);
+quickSort(numbers, 0, numbers.length-1);
+console.log(numbers);
