cd2bad79e68dfa1b535ee1c71b52eeefe54c8079
christian soto <cjsotoparra@email.arizona.edu>
2020-07-06 10:21:04 -0700

    implemented BFS on BST

---
+++ BFS_DFS/main.js
@@ -0,0 +1,21 @@
+const BinarySearchTree = require("./bst");
+
+const myBST = new BinarySearchTree();
+
+myBST.insert(9);
+myBST.insert(4);
+myBST.insert(6);
+myBST.insert(20);
+myBST.insert(170);
+myBST.insert(15);
+myBST.insert(1);
+//console.log(myBST.remove(9));
+//console.log(JSON.stringify(traverse(myBST.root)))
+
+console.log(myBST.bFS())
+function traverse(node) {
+  	const tree = { value: node.value };
+  	tree.left = node.left === null ? null : traverse(node.left);
+  	tree.right = node.right === null ? null : traverse(node.right);
+  	return tree;
+}
